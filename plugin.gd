a2c08b71bb0c2b041afa8367b36d69b72ccb7845
j-c7 <7jaydev@protonmail.com>
2025-03-28 19:16:10 -0600

    Changed skydome to sky shaders #1

--- plugin.gd
+++ plugin.gd
@@ -1,8 +1,8 @@
 @tool
 extends EditorPlugin
 
-const _skydome_script = preload("res://addons/universal-sky/src/sky/dome/usky_dome_3d.gd")
-const _skydome_icon = preload("res://addons/universal-sky/assets/icons/Sky.svg")
+const _sky_script = preload("res://addons/universal-sky/src/sky/sky-manager/usky3d.gd")
+const _sky_icon = preload("res://addons/universal-sky/assets/icons/Sky.svg")
 
 const _sun_script = preload("res://addons/universal-sky/src/sky/celestials/usky_sun_3d.gd")
 const _sun_icon = preload("res://addons/universal-sky/assets/icons/sun.svg")
@@ -11,12 +11,11 @@ const _moon_script = preload("res://addons/universal-sky/src/sky/celestials/usky
 const _moon_icon = preload("res://addons/universal-sky/assets/icons/moon.svg")
 
 func _enter_tree() -> void:
-	add_custom_type("USkyDome3D", "Node3D", _skydome_script, _skydome_icon)
+	add_custom_type("USky3D", "Node3D", _sky_script, _sky_icon)
 	add_custom_type("USkySun3D", "DirectionalLight3D", _sun_script, _sun_icon)
 	add_custom_type("USkyMoon3D", "DirectionalLight3D", _moon_script, _moon_icon)
-	print("PENETRE")
 
 func _exit_tree() -> void:
-	remove_custom_type("USkyDome3D")
+	remove_custom_type("USky3D")
 	remove_custom_type("USkySun3D")
 	remove_custom_type("USkyMoon3D")

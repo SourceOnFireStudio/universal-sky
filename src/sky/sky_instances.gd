02946c7d6beca4476f9b7166de2f346805d34b1d
j-c7 <7jaydev@protonmail.com>
2025-09-01 12:15:24 -0600

    Added instance added and removed signals

--- src/sky/sky_instances.gd
+++ src/sky/sky_instances.gd
@@ -7,6 +7,12 @@
 @tool
 extends Node
 
+signal pre_instance_added
+signal pre_instance_removed
+
+signal instance_added
+signal instance_removed
+
 var instances: Array[SkyHandler]:
 	get: return instances
 
@@ -14,7 +20,11 @@ var current_instance: SkyHandler:
 	get: return instances[0]
 
 func set_instance(p_instance: SkyHandler) -> void:
+	pre_instance_removed.emit()
 	instances.push_back(p_instance)
+	instance_added.emit()
 
 func remove_instance(p_instance: SkyHandler) -> void:
+	pre_instance_removed.emit()
 	instances.erase(p_instance)
+	instance_removed.emit()
